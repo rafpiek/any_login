042807dece71971c977d409b525c2e150d81f0d0
Rafał Piekara <rafpiek@gmail.com>
2026-06-24 12:37:28 +0200

    Redesign dev UI as floating panel (Next.js devtools style)
    Replace full-width purple bar with a corner FAB and popover panel:
    dark zinc surface, structured fields, session card, recent users.
    Add click-outside and Escape to close. Honor login_on for select/id/both.
    Fix duplicate nested any_login_id_input definition in helpers.

--- lib/any_login/helpers.rb
+++ lib/any_login/helpers.rb
@@ -9,9 +9,7 @@ module AnyLogin
     if AnyLogin.enabled
 
       def any_login_id_input
-        def any_login_id_input
-          text_field_tag :id, '', placeholder: 'ID', id: 'any_login_id_input', required: true
-        end
+        text_field_tag :id, "", placeholder: "Paste user ID…", id: "any_login_id_input", required: true, autocomplete: "off"
       end
 
       def any_login_submit
@@ -36,16 +34,15 @@ module AnyLogin
         users = ids.collect do |id|
           AnyLogin.klass.where(AnyLogin.klass.primary_key => id).first
         end.compact
-
         collection = AnyLogin::Collection.new(users).to_a
         if collection.any?
           select_options = options_for_select(collection)
-          [
-            content_tag(:span, id: 'anylogin_back_to_user') do
-              "History: "
-            end,
-            select_tag(:back_to_previous_id, select_options, select_html_options("Back to:"))
-          ].join.html_safe
+          content_tag(:div, class: "any_login_history_field") do
+            safe_join([
+              content_tag(:span, "Recent", class: "any_login_history_label"),
+              select_tag(:back_to_previous_id, select_options, select_html_options("Switch back to…"))
+            ])
+          end
         end
       end
 
@@ -68,12 +65,13 @@ module AnyLogin
       def current_user_information
         if respond_to?(AnyLogin.provider.constantize::Controller.any_login_current_user_method) &&
            user = send(AnyLogin.provider.constantize::Controller.any_login_current_user_method)
-          content_tag :span, :class => 'any_login_user_information' do
-            if AnyLogin.name_method.is_a?(Symbol)
-              raw("Current #{AnyLogin.klass_name}: #{h(user.send(AnyLogin.name_method)[0])} &mdash; ID: #{user.id}")
-            else
-              raw("Current #{AnyLogin.klass_name}: #{h(AnyLogin.name_method.call(user)[0])} &mdash; ID: #{user.id}")
-            end
+          label, _id = if AnyLogin.name_method.is_a?(Symbol)
+            user.send(AnyLogin.name_method)
+          else
+            AnyLogin.name_method.call(user)
+          end
+          content_tag :span, class: "any_login_user_information" do
+            raw("Signed in as <strong>#{h(label)}</strong><br><span>ID: #{h(user.id)}</span>")
           end
         end
       end
